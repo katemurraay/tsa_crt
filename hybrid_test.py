72f88c620ac96ef88bc424501acf22a8012c4747
Kate Murray <117349221@umail.ucc.ie>
2022-12-08 20:48:37 +0000

    fix name

--- hybrid_test.py
+++ hybrid_test.py
@@ -11,7 +11,7 @@ def main():
     retrain = [0, 30, 31, 31, 30, 31, 30, 31, 31, 28, 31, 30]
     outputs =[30, 31, 31, 30, 31, 30, 31, 31, 28, 31, 30, 31] 
     scaling = ['minmax']
-    tuned =  0
+    tuned =  1
     window = 30
     for t in targets:
         for c in cryptos:
@@ -21,7 +21,7 @@ def main():
             for index, r in enumerate(retrain):
                 output = outputs[index]
                 
-                experiment_name = 'hybrid-' + c + '-' + t + '-h' + str(h) + '-'+ str(index) +'m'
+                experiment_name = 'hybrid-' + c + '-' + t  + '-w' + str(window) +  '-h' + str(h) + '-' + str(len(outputs)) +'m'
                 ds = dataset_binance.BinanceDataset(filename='crypto_task_' + c + '.csv', input_window=window, output_window=1,
                                                             horizon=h, training_features=['close'],
                                                             target_name=['close'], train_split_factor=0.8)
